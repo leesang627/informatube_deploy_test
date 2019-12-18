d701bbb293a7654cb132159154704074ab996c98
Sangwook Lee <leesang627@gmail.com>
2019-12-19 01:42:53 +0900

    Updates

---
+++ precache-manifest.ca014f9907f3add477f4275a208f53ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3643d96ab534a3d3172264c8dbc24b4",
+    "url": "/informatube_deploy_test/index.html"
+  },
+  {
+    "revision": "a7384e21099ed75e17ea",
+    "url": "/informatube_deploy_test/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b599143541b094552830",
+    "url": "/informatube_deploy_test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7384e21099ed75e17ea",
+    "url": "/informatube_deploy_test/static/js/2.e4e85ef7.chunk.js"
+  },
+  {
+    "revision": "1e9ae14cfce1a12c0dbc6f3ec4ec92f7",
+    "url": "/informatube_deploy_test/static/js/2.e4e85ef7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b599143541b094552830",
+    "url": "/informatube_deploy_test/static/js/main.6d9e52da.chunk.js"
+  },
+  {
+    "revision": "8bb833bc523097cd68a4",
+    "url": "/informatube_deploy_test/static/js/runtime-main.c5d9cea2.js"
+  }
+]);
