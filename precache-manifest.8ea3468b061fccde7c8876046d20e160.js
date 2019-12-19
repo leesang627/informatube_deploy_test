cc549be3188ead70d69256f4f30020b42d057af9
Sangwook Lee <leesang627@gmail.com>
2019-12-19 15:46:31 +0900

    Updates

--- precache-manifest.8ea3468b061fccde7c8876046d20e160.js
+++ precache-manifest.8ea3468b061fccde7c8876046d20e160.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "089b34f4dc95ba41268b89e043d9c5ec",
+    "revision": "407808ab72261f66768c3082929b484c",
     "url": "/informatube_deploy_test/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/informatube_deploy_test/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "651e24fb377d490051da",
+    "revision": "b5c8c49afe9da4ea61d6",
     "url": "/informatube_deploy_test/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/informatube_deploy_test/static/js/2.25587dea.chunk.js.LICENSE"
   },
   {
-    "revision": "651e24fb377d490051da",
-    "url": "/informatube_deploy_test/static/js/main.4398b9ce.chunk.js"
+    "revision": "b5c8c49afe9da4ea61d6",
+    "url": "/informatube_deploy_test/static/js/main.81f71c46.chunk.js"
   },
   {
     "revision": "8bb833bc523097cd68a4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/informatube_deploy_test/precache-manifest.ed7353904cf19c6642bde9e8e373ea80.js"
+  "/informatube_deploy_test/precache-manifest.8ea3468b061fccde7c8876046d20e160.js"
 );
 
 self.addEventListener('message', (event) => {
